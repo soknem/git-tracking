d0c9b52bcb3fccdab6b801580ff6bda7b5773a4f
povsoknem <povsoknem@gmail.com>
2024-06-06 11:58:27 +0700

    Auto commit

--- src/main/java/git/treacking/push/PushApplication.java
+++ src/main/java/git/treacking/push/PushApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PushApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PushApplication.class, args);
 	}
